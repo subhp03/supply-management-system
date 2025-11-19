93f0efa8754dcb99207e3d47c661461c18e2f3a3
Subhrabaran P <subhrabaranpanigrahi@gmail.com>
2025-11-19 23:36:18 +0530

    WIP: SignIn Page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@stackframe/stack";
+
+export default function signinpage(){
+    return (
+        <div className="min-h-screen flex items-center justify-center ">
+            <div>
+                <SignIn/>
+            </div>
+        </div>
+    )
+}
