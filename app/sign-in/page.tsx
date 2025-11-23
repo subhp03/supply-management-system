0405f10e0602c040ae90b0a014e584678b4312dd
Subhrabaran P <subhrabaranpanigrahi@gmail.com>
2025-11-23 23:49:04 +0530

    Add styling to Sign-In Page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,11 +1,13 @@
 import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
 
 export default function signinpage(){
     return (
-        <div className="min-h-screen flex items-center justify-center ">
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black flex items-center justify-center ">
             <div>
-                <SignIn/>
+                <SignIn />
+                <Link href="/">Home</Link>
             </div>
         </div>
-    )
+    );
 }
