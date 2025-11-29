089f04aa492cbe847421baa2bd5e7225f04fd067
Subhrabaran P <subhrabaranpanigrahi@gmail.com>
2025-11-30 00:28:54 +0530

    Initialize sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,10 @@
+export default function Sidebar({
+    currentpath ="/dashboard",
+}: {
+    currentpath: string;
+}) {
+
+    return (
+    <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen p-6 z-10"></div>
+    );
+}
